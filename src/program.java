c690429133c8f57820380aef9cf3fed854154fd3
Gustavopomp <gustavoferrazpompeo@gmail.com>
2023-03-08 15:08:03 -0300

    usando o file

--- src/program.java
+++ src/program.java
@@ -1,9 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class program {
 
 	public static void main(String[] args) {
 	
 
+		File file =  new File("X:\\desenv\\Arquvos\\in.txt");
+		Scanner ler = null;
+		try {
+			ler = new Scanner(file);
+			while(ler.hasNextLine()) {
+				System.out.println(ler.nextLine());
+			}
+			
+		}catch(IOException e) {
+			System.out.println("Error : " + e.getMessage());
+		}
+		finally {
+			if (ler!= null)
+				ler.close();
+		}
 	}
 
 }
